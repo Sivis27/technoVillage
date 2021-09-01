890896484efe6900443fe6d15feee806ab8edc09
vimaladevi <atom.vimaladevi@gmail.com>
2021-09-01 08:56:33 +0530

    Boot App

---
+++ springBootVilla/src/main/java/com/vila/springBootVilla/SpringBootVillaApplication.java
@@ -0,0 +1,13 @@
+package com.vila.springBootVilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVillaApplication.class, args);
+	}
+
+}
